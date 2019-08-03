f76badb00b53375951dce9e39fe9f49d77ed44d4
dane <daneasc@gmail.com>
2019-08-03 10:08:07 +0200

    added page not found

---
+++ vueschool-forum/src/pages/PageNotFound.vue
@@ -0,0 +1,30 @@
+<template>
+    <div 
+    class="col-full" 
+    :class="$style.centered"
+    >
+
+        <h1 :class="$style.headingLarge">Page not found</h1>
+        <p :class="$style.textLarge">
+            Rather 
+            <router-link :to="{name: 'Home'}">go home...</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style module>
+    .headingLarge {
+        font-size: 100px;
+    }
+    .textLarge {
+        font-size: 50px;
+        font-weight: 100;
+    }
+    .centered {
+        text-align: center
+    }
+</style>
